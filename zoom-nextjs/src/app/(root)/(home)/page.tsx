bcedef384d10f3406ebe906cdef1300405373dbd
Roy <iRoySwift@iCloud.com>
2024-04-09 11:45:09 +0800

    home page

--- zoom-nextjs/src/app/(root)/(home)/page.tsx
+++ zoom-nextjs/src/app/(root)/(home)/page.tsx
@@ -1,10 +1,34 @@
+import MeetingTypeList from "@/components/MeetingTypeList";
 import React from "react";
 
 interface Props {}
 const Home: React.FC<Props> = () => {
+    const now = new Date();
+    const time = now.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+    });
+    const date = new Intl.DateTimeFormat("zh-CH", { dateStyle: "full" }).format(
+        now
+    );
     return (
         <section className="flex size-full flex-col gap-10 text-white">
-            <h1 className="text-3xl font-bold">Home</h1>
+            <div className="bg-hero h-[300px] w-full rounded-[20px] bg-cover">
+                <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+                    <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+                        Upcoming Meeting at: 12:00PM
+                    </h2>
+                    <div className="flex flex-col gap-2">
+                        <h1 className="text-4xl font-extrabold lg:text-7xl">
+                            {time}
+                        </h1>
+                        <p className="text-sky-1 text-lg font-medium lg:text-2xl">
+                            {date}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <MeetingTypeList />
         </section>
     );
 };
