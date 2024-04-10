77cd0db2fe1162dfb62107fc68fe27c3fd02053c
Roy <iRoySwift@iCloud.com>
2024-04-10 14:31:34 +0800

    stream create meeting

--- zoom-nextjs/src/app/(root)/layout.tsx
+++ zoom-nextjs/src/app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import StreamClientProvider from "@/providers/StreamClientProvider";
 import React from "react";
 
 interface Props {
-    children: React.ReactNode
+    children: React.ReactNode;
 }
-const RootLayout: React.FC<Props> = ({children}) => {
-    return <main>
-        {children}
-    </main>;
+const RootLayout: React.FC<Props> = ({ children }) => {
+    return (
+        <main>
+            <StreamClientProvider>{children}</StreamClientProvider>
+        </main>
+    );
 };
 export default RootLayout;
