4e66eef1c512dd96a5458f6dfc02b2077a22300b
Roy <iRoySwift@iCloud.com>
2024-04-28 15:08:27 +0800

    get post

--- ordering-nest/apps/orders/src/orders.controller.ts
+++ ordering-nest/apps/orders/src/orders.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { CreateOrderRequest } from './dto/create-order-request';
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Get()
   getHello(): string {
     return this.ordersService.getHello();
+  }
+
+  @Get('ids')
+  async getOrders() {
+    return this.ordersService.getOrders();
+  }
+
+  @Get('order/:id')
+  async getOrdersById(@Param('id') id: string) {
+    return this.ordersService.getOrdersById(id);
+  }
+
+  @Get('orderId')
+  async getOrdersId(@Query() query) {
+    return this.ordersService.getOrdersById(query.id);
+  }
+
+  @Post()
+  async createOrder(@Body() request: CreateOrderRequest) {
+    return this.ordersService.createOrder(request);
   }
 }
--- ordering-nest/apps/orders/src/orders.service.ts
+++ ordering-nest/apps/orders/src/orders.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { CreateOrderRequest } from './dto/create-order-request';
+import { OrderRepository } from './order.repository';
 
 @Injectable()
 export class OrdersService {
+  constructor(private readonly orderRepository: OrderRepository) {}
+
   getHello(): string {
-    return 'Hello World! orders';
+    return 'Hello World! orders2';
+  }
+
+  async createOrder(request: CreateOrderRequest) {
+    return this.orderRepository.create(request);
+  }
+
+  async getOrders() {
+    return this.orderRepository.find({});
+  }
+
+  async getOrdersById(id: string) {
+    return this.orderRepository.findOne({ _id: id });
   }
 }
