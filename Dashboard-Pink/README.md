ce6d241143bdf6c119ccb62ac438461277d4d1d3
Roy <iRoySwift@iCloud.com>
2025-05-09 22:08:14 +0800

    dashboard init

---
+++ Dashboard-Pink/README.md
@@ -0,0 +1,15 @@
+# React Dashboard Admin
+
+![React Dashboard admin](./demo.png)
+
+# Bootstrap
+
+```
+pnpm i
+```
+
+# Start
+
+```
+pnpm start
+```
---
+++ Dashboard-Pink/src/App.js
@@ -0,0 +1,18 @@
+import SideBar from './components/SideBar/SideBar';
+import './App.css';
+import MainDash from './components/MainDash/MainDash';
+import RightSide from './components/RightSide/RightSide';
+
+function App () {
+  return (
+    <div className="App">
+      <div className="AppGlass">
+        <SideBar />
+        <MainDash />
+        <RightSide />
+      </div>
+    </div>
+  );
+}
+
+export default App;
